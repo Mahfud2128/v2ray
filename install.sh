4fe4592f90635b16181d0a05597cf7710636a054
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2021-04-09 16:36:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 source <(curl -sL https://raw.githubusercontent.com/natxanss/multi-v2ray/master/v2ray.sh)
 apt install git
 pip install v2ray-util
-cd /usr/local/lib/python3.5/dist-packages && rm -R v2ray_util && git clone https://github.com/natxanss/multi-v2ray/tree/master/v2ray_util
+cd /usr/local/lib/python3.5/dist-packages && rm -R v2ray_util && git clone https://github.com/natxanss/multi-v2ray && cp -r /usr/local/lib/python3.5/dist-packages/multi-v2ray/v2ray_util /usr/local/lib/python3.5/dist-packages && rm -r multi-v2ray
 v2ray restart
