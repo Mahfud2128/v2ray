e175f517d9ea776a45f12fd7be0b21e1ca7a90c8
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2021-04-11 19:27:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+#V2RAY PREMIUM SCRIPT BY XANS TECH STORE
 
-source <(curl -sL https://raw.githubusercontent.com/natxanss/multi-v2ray/master/v2ray.sh)
-apt install git
-pip install v2ray-util
-cd /usr/local/lib/python3.5/dist-packages && rm -R v2ray_util && git clone https://github.com/natxanss/multi-v2ray && cp -r /usr/local/lib/python3.5/dist-packages/multi-v2ray/v2ray_util /usr/local/lib/python3.5/dist-packages && rm -r multi-v2ray
-v2ray restart
-rm install.sh
+#Install Variable
+echo -e "Pastikan domain sudah dipersiapkan untuk vmess"
+read -p "Domain: " domain
+
+#Checking Dependencies
+apt-get install git
+apt-get install wget
+apt-get install uuid
+
+#Make installation for v2ray
+#Thanks to Jhrory
+source <(curl -sL https://multi.netlify.app/v2ray.sh)
+
+#Ubah Config bawaan
+cd /etc/v2ray/config.json && rm config.json && wget https://raw.githubusercontent.com/natxanss/v2ray/main/config.json
+cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/data.json
+cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/domain.json
+cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/user.txt
+
+#Ubah Domain
+var='"Host"'
+ler='"'
+
+sed -i "s/dom/$domain/" /etc/v2ray/domain.json
+sed -i "s/$var/$var: $ler$domain$ler/" /etc/v2ray/config.json
+
+#Download Command
+wget -O /usr/bin/addv2ray "https://raw.githubusercontent.com/natxanss/v2ray/main/addv2ray.sh"
+wget -O /usr/bin/listv2ray "https://raw.githubusercontent.com/natxanss/v2ray/main/listv2ray.sh"
+
+rm /usr/bin/v2ray
+systemctl restart v2ray
