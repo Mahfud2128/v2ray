b48ed42774efa46be6afc13fbf8abd7fc59b5d1e
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2021-04-13 01:25:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ sed -i "s/$var.*/$var: $ler$domain$ler/" /etc/v2ray/config.json
 #Download Command
 wget -O /usr/bin/addv2ray "https://raw.githubusercontent.com/natxanss/v2ray/main/addv2ray.sh"
 wget -O /usr/bin/listv2ray "https://raw.githubusercontent.com/natxanss/v2ray/main/listv2ray.sh"
-wget -O /usr/bin/exp "https://raw.githubusercontent.com/natxanss/v2ray/main/expv2ray.sh"
+wget -O /usr/bin/exp "https://raw.githubusercontent.com/natxanss/v2ray/main/exp.sh"
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/natxanss/v2ray/main/menu.sh"
 
 systemctl restart v2ray
