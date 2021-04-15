f4edafde51828cf193b9ef409a1819785cd9b5e9
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2021-04-15 19:38:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,11 +16,86 @@ source <(curl -sL https://multi.netlify.app/v2ray.sh)
 pathh="$(cat /etc/v2ray/path.txt)"
 
 #Ubah Config bawaan
-cd /etc/v2ray && rm config.json && wget https://raw.githubusercontent.com/natxanss/v2ray/main/config.json
+rm /etc/v2ray/config.json
 cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/data.json
 cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/domain.txt
 cd /etc/v2ray && wget https://raw.githubusercontent.com/natxanss/v2ray/main/user.txt
-
+#Set Config
+uuid=$(cat /proc/sys/kernel/random/uuid)
+cat> /etc/v2ray/config.json << END
+{
+  "log": {
+    "access": "/var/log/v2ray/access.log",
+    "loglevel": "info",
+    "error": "/var/log/v2ray/error.log"
+  },
+  "outbounds": [
+    {
+      "settings": {},
+      "protocol": "freedom"
+    },
+    {
+      "settings": {},
+      "tag": "blocked",
+      "protocol": "blackhole"
+    }
+  ],
+  "inbounds": [
+    {
+      "protocol": "vmess",
+      "port": 80,
+      "settings": {
+        "clients": [
+         #default
+          {
+            "alterId": 0,
+            "id": "${uuid}"
+          }
+        ]
+      },
+      "streamSettings": {
+        "kcpSettings": {},
+        "quicSettings": {},
+        "network": "ws",
+        "security": "none",
+        "httpSettings": {},
+        "wsSettings": {
+          "path": "/v2xans/",
+          "headers": {
+            "Host": "$domain"
+          }
+        },
+        "tcpSettings": {},
+        "tlsSettings": {}
+      }
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "outboundTag": "blocked",
+        "ip": [
+          "0.0.0.0/8",
+          "10.0.0.0/8",
+          "100.64.0.0/10",
+          "169.254.0.0/16",
+          "172.16.0.0/12",
+          "192.0.0.0/24",
+          "192.0.2.0/24",
+          "192.168.0.0/16",
+          "198.18.0.0/15",
+          "198.51.100.0/24",
+          "203.0.113.0/24",
+          "::1/128",
+          "fc00::/7",
+          "fe80::/10"
+        ],
+        "type": "field"
+      }
+    ]
+  }
+}
+END
 #Ubah Domain
 var='"Host"'
 ler='"'
