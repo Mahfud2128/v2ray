6b3c81613a4f3fa188b9320678536c04adee39fa
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2021-04-17 03:17:32 +0700

    Update delv2ray.sh

--- delv2ray.sh
+++ delv2ray.sh
@@ -4,11 +4,11 @@ listv2ray
 echo -e "Masukan Username yang ingin dihapus"
 read -p "Username: " user
 
-if grep -qc "$user" /etc/v2ray/config.json
+if grep -qc "${user}-XTC" /etc/v2ray/config.json
 then
-        sed -i -e "/#$user.*/,/$user.*$/d" /etc/v2ray/config.json
-        sed -i -e "/#$user.*/,/$user.*$/d" /etc/v2ray/tls.json
-        sed -i -e "/#$user.*/d" /etc/v2ray/user.txt
+        sed -i -e "/#${user}-XTC.*/,/$user.*$/d" /etc/v2ray/config.json
+        sed -i -e "/#${user}-XTC.*/,/$user.*$/d" /etc/v2ray/tls.json
+        sed -i -e "/#${user}-XTC.*/d" /etc/v2ray/user.txt
         systemctl restart v2ray
         listv2ray
         echo ""
@@ -17,5 +17,6 @@ else
         echo ""
         echo -e "User tidak ada!"
         systemctl restart v2ray
+        systemctl restart v2tls
         exit
 fi
